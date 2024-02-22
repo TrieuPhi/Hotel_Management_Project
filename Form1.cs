1507efb0474a58486b4fee811b1b3bc2f394f056
Dai Trieu Phi <dtptrieuphidtp@gmail.com>
2024-02-22 14:09:33 +0700

    create ui form Login

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Quan_Ly_Khach_San_2024
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
